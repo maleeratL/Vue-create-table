d17485898b20d863647e3e188d0dc1d6c5414a18
Maleerat Lertruedeeporn <maleerat.l.123@gmail.com>
2019-11-26 23:10:46 +1300

    create EmployeeTable component

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="employee-table">
+        <table>
+            <thead>
+                <tr>
+                    <th>Employee name</th>
+                    <th>Employee email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Kate Smith</td>
+                    <td>kate-smith@gmail.com</td>
+                </tr>
+                <tr>
+                    <td>John Lead</td>
+                    <td>jlead@gmail.com</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "employee-table"
+    }
+</script>
+
+<style scoped>
+
+</style>
